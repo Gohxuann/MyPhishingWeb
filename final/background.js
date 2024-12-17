918e9960c2dd288e3dad3b062480d91db1f7b50f
Jaslynnn64 <122892635+Jaslynnn64@users.noreply.github.com>
2024-12-17 23:27:57 +0800

    update

--- final/background.js
+++ final/background.js
@@ -1,60 +1,38 @@
-// Listen for downloads
-chrome.downloads.onCreated.addListener((downloadItem) => {
-  console.log("File Download Detected:", downloadItem.filename);
-
-  // Send the downloaded file's URL to your backend for scanning
-  if (downloadItem.url) {
-      fetch("http://127.0.0.1:5000/api", {
-          method: "POST",
-          body: JSON.stringify({ file: downloadItem.url }),
-          headers: {
-              "Content-Type": "application/json"
-          }
-      })
-      .then(response => response.json())
-      .then(data => {
-          console.log("Scan Result:", data);
-
-          if (data.malicious === "yes") {
-              // Show an alert notification if file is malicious
-              chrome.notifications.create({
-                  type: "basic",
-                  iconUrl: "icon.png",
-                  title: "Malicious File Detected!",
-                  message: `The file "${downloadItem.filename}" is flagged as malicious. Please delete it immediately.`
-              });
-          } else {
-              chrome.notifications.create({
-                  type: "basic",
-                  iconUrl: "icon.png",
-                  title: "File Scan Result",
-                  message: `The file "${downloadItem.filename}" appears clean.`
-              });
-          }
-      })
-      .catch(error => {
-          console.error("Error scanning file:", error);
-      });
-  }
-});
-
 // Existing tab listener for URL scanning
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.url) {
-      console.log("URL Changed to:", changeInfo.url);
+    if (changeInfo.url) {
+        console.log("URL Changed to:", changeInfo.url);
 
-      fetch("http://127.0.0.1:5000/api?url=" + encodeURIComponent(changeInfo.url))
-          .then(response => response.json())
-          .then(data => {
-              console.log("Backend Response:", data);
-
-              chrome.notifications.create({
-                  type: "basic",
-                  iconUrl: "icon.png",
-                  title: "URL Scan Result",
-                  message: JSON.stringify(data, null, 2)
-              });
-          })
-          .catch(error => console.error("Error:", error));
-  }
+        // Send the URL to the backend server for scanning
+        fetch(`http://127.0.0.1:5000/api?url=${encodeURIComponent(changeInfo.url)}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log("Scan Result:", data);
+                // If the URL is malicious, show an alert notification
+                if (data.malicious === "yes") {
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: "icon.png",
+                        title: "Malicious URL Detected!",
+                        message: `Warning! The URL "${changeInfo.url}" is flagged as malicious.`
+                    });
+                } else {
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: "icon.png",
+                        title: "URL Scan Result",
+                        message: `The URL "${changeInfo.url}" appears clean.`
+                    });
+                }
+            })
+            .catch(error => {
+                console.error("Error scanning URL:", error);
+                chrome.notifications.create({
+                    type: "basic",
+                    iconUrl: "icon.png",
+                    title: "Scan Error",
+                    message: "Unable to scan the URL. Check backend connection."
+                });
+            });
+    }
 });
